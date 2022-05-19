c0f6f0096ea7b07fd463f60f54dfe44a9d4fac44
altayozyavas <altayozyavas@gmail.com>
2022-05-19 18:10:36 -0500

    add second solution

--- javascript/exercises/bmiCalc/script.js
+++ javascript/exercises/bmiCalc/script.js
@@ -1,3 +1,4 @@
+// Solution 1 -> onclick event attribute
 function calcBmi(){
     const height = document.getElementById("height").value;
     const weight = document.getElementById("weight").value;
@@ -8,3 +9,16 @@ function calcBmi(){
         document.getElementById("results").innerHTML = bmi;
     }
 }
+
+// Solution 2 -> eventListener
+const calc = document.getElementById("btn");
+calc.addEventListener("click", () => {
+    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value;
+    const bmi = weight / ((height*height)/10000);
+    if (isNaN(bmi)) {
+        document.getElementById("results").innerHTML = "Please enter number";
+    } else {
+        document.getElementById("results").innerHTML = bmi;
+    }
+})
