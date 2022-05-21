34d5a142f80f0568cb34ec9de24c8f2b21226afb
altayozyavas <altayozyavas@gmail.com>
2022-05-20 23:45:14 -0500

    add keyCode exercise

--- javascript/exercises/may17/project1/script.js
+++ javascript/exercises/may17/project1/script.js
@@ -4,17 +4,24 @@
 // - Whenever i keypress on any keyboard take an action based on listed above keycodes and change 
 // something inside the box (example: change color etc.)
 
+const ids = [81, 87, 69, 65, 83, 68];
 const body = document.getElementsByTagName("body")[0];
-const box = document.getElementById("box");
+const allButtons = document.getElementsByTagName("button");
+const messageBox = document.getElementById("message");
+const errMessage = "Wrong input, try again!";
+const successMessage = "Nice Choice!";
+const green = "green";
 
-body.addEventListener("keypress", (event) => {
-    if (event.keyCode === 13) {
-        // box.innerHTML = "";
-        console.log("Enter clicked");
-        box.innerHTML = `${event.keyCode} clicked`;
-        // console.log(String.fromCharCode(13));
-        // console.log(keyboardMap[13]);
-    }
+body.addEventListener("keydown", (event) => {
+    styleAllBtns("none", successMessage);
+    const k = event.keyCode;
+    (ids.includes(k)) ? document.getElementById(k).style.background = green : styleAllBtns("red", errMessage);
 });
 
-// const btn = document.getElementById("box");
+function styleAllBtns(color, message) {
+    (message == successMessage) ? messageBox.style.color = green : messageBox.style.color = color;
+    messageBox.innerHTML = message;
+    for (let i = 0; i < allButtons.length; i++) {
+        allButtons[i].style.background = color;
+    }
+}
