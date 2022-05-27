31e22f47a913164f555d9f2073e108da623beef0
altayozyavas <altayozyavas@gmail.com>
2022-05-27 13:46:36 -0500

    add second solution

--- javascript/exercises/promise/index.js
+++ javascript/exercises/promise/index.js
@@ -1,4 +1,5 @@
 const usersHtml = document.getElementById("users");
+const usersHtml2 = document.getElementById("users2");
 
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(response => response.json())
@@ -13,3 +14,27 @@ fetch("https://jsonplaceholder.typicode.com/users")
     });
 })
 .catch(err => console.log(err));
+
+//Second solution with async functions, it takes longer but keeps the order because I used for of loop. For each is not suitable for async and waits
+async function getPostById(id) {
+    try {
+        const allPosts = await (await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)).json();
+        return await allPosts[0].body;
+    } catch (error) {
+        console.log("Failed to retrieve post", error)
+    }
+}
+
+async function getUserInfo() {
+    try {
+        const users = await (await fetch(`https://jsonplaceholder.typicode.com/users`)).json();
+        for (const usr of users) {
+            const post = await getPostById(usr.id);
+            usersHtml2.innerHTML += `${usr.id}: ${usr.name} / ${usr.email}<br/>Post: ${post}<br/><br/>`;
+        }
+    } catch (error) {
+        console.log("Failed to retrieve post", error)
+    }
+}
+
+getUserInfo();
